b208b756228c0025573659bbd08f0607de28ec5d
vitaliAI <vitali_mueller@yahoo.de>
2018-03-17 17:32:46 +0000

    Updated home html to bootstrap album example

--- Portfolio/portfolio/blog/models.py
+++ Portfolio/portfolio/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Add the Blog app to the settings
+
+
+# Creating a migration
+
+
+# Migrate
+
+# Add to the admin
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
