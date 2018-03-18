3bc4abc9c24e9a7cdc070efd20b4e77a0e46040c
vitaliAI <vitali_mueller@yahoo.de>
2018-03-18 19:29:53 +0000

    Added Blog Details

--- Portfolio/portfolio/blog/views.py
+++ Portfolio/portfolio/blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
+from .models import Blog
+
+def all_blogs(request):
+    blogs = Blog.objects.all()
+    return render(request, 'blog/all_blogs.html', {'blogs':blogs})
+
+def detail(request, blog_id):
+    detail_blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'detail_blog':detail_blog})
