dce4999bf3af7896f38d4cec24a801f94906cc06
vitaliAI <vitali_mueller@yahoo.de>
2018-03-20 21:45:12 +0000

    Created sign up page

--- HuntCloneApp/huntclone/accounts/views.py
+++ HuntCloneApp/huntclone/accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        # User has informatio and want to create an account
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'User has already been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error': 'Password was typed incorrect'})
+
+    else:
+        return render(request, 'accounts/signup.html', {})
+
+
+def login(request):
+    return render(request, 'accounts/login.html', {})
+
+
+def logout(request):
+    return render(request, 'accounts/login.html', {})
